10a58b04965338eea8c82afaf3bdc55c26898e81
Grasp <hunter.wxhu@gmail.com>
2011-12-21 17:58:22 +0800

    merge

---
+++ lib/tasks/load_cookie.rb
@@ -0,0 +1,21 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+def load_cookie
+#  cookie_dir ="D:\\Profiles\\w22812\\Application Data\\Mozilla\\Firefox\\Profiles\\623tc49u.default"  
+ 
+cookie_dir ="/home/hunter/tttk3240"
+ cookie = String.new  
+  Dir.chdir(cookie_dir){|dir|  
+    db = SQLite3::Database.new('cookies.sqlite')  
+    p = Proc.new{|s| s.to_i.zero? ? 'TRUE' : 'FALSE'}  
+    db.execute("SELECT  host, isHttpOnly, path, isSecure, expiry, name, value FROM moz_cookies   
+    ORDER BY id DESC"){|r|  
+      cookie << [r[0], p.call(r[1]), r[2], p.call(r[3]), r[4], r[5], r[6]].join("\t") << "\n"  
+    }  
+  }  
+  $cookie= cookie
+  return cookie
+end
+
+load_cookie
